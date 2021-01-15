323e8b353f35e7c32b6a3678ff8b1c3244ac9270
Eternal <1872642516@qq.com>
2021-01-15 15:59:35 +0800

    修改图片地址

--- src/views/tree.vue
+++ src/views/tree.vue
@@ -14,37 +14,37 @@
             return {
                 treeData:{
                     name: 'root',
-                    image_url: "https://static.refined-x.com/avat.jpg",
+                    image_url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
                     children: [
                         {
                             name: 'children1',
-                            image_url: "https://static.refined-x.com/avat1.jpg"
+                            image_url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
                         },
                         {
                             name: 'children2',
-                            image_url: "https://static.refined-x.com/avat2.jpg",
+                            image_url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
                             mate: {
                                 name: 'mate',
-                                image_url: "https://static.refined-x.com/avat3.jpg"
+                                image_url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpegg"
                             },
                             children: [
                                 {
                                     name: 'grandchild',
-                                    image_url: "https://static.refined-x.com/avat.jpg"
+                                    image_url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
                                 },
                                 {
                                     name: 'grandchild2',
-                                    image_url: "https://static.refined-x.com/avat1.jpg"
+                                    image_url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
                                 },
                                 {
                                     name: 'grandchild3',
-                                    image_url: "https://static.refined-x.com/avat2.jpg"
+                                    image_url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
                                 }
                             ]
                         },
                         {
                             name: 'children3',
-                            image_url: "https://static.refined-x.com/avat.jpg"
+                            image_url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
                         }
                     ]
                 }
